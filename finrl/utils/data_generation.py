5096f45784b51137d39f442484e9240bb3b27f3c
Anton Chernov <tony.pitchblack@yandex.ru>
2025-01-26 12:18:50 +0300

    add finetune option for data generation

--- finrl/utils/data_generation.py
+++ finrl/utils/data_generation.py
@@ -1,7 +1,7 @@
 from calendar import monthrange
 import pandas as pd
 
-def generate_quarterly_date_ranges(train_start_date, min_test_start_date, max_test_end_date, preproc_df, return_strings=False):
+def generate_quarterly_date_ranges(train_start_date, min_test_start_date, max_test_end_date, return_strings=False, finetune_previous_val=False):
     is_quarter_start = lambda date: date.month in [1, 4, 7, 10] and date.day == 1
 
     min_test_start_date = pd.Timestamp(min_test_start_date)
@@ -11,11 +11,10 @@ def generate_quarterly_date_ranges(train_start_date, min_test_start_date, max_te
     assert is_quarter_start(train_start_date), f"train_start_date {train_start_date} is not a quarter start date."
     assert is_quarter_start(min_test_start_date), f"min_test_start_date {min_test_start_date} is not a quarter start date."
 
-    assert max_test_end_date + pd.DateOffset(month=3) <= preproc_df['date'].max()
-    assert train_start_date + pd.DateOffset(days=1) >= preproc_df['date'].min()
-
     test_start_date = min_test_start_date
     date_ranges = []
+    full_train_start_date = train_start_date
+
     while True:
         val_start_date = test_start_date - pd.DateOffset(months=3)
         test_end_date = test_start_date + pd.DateOffset(months=3)
@@ -23,12 +22,19 @@ def generate_quarterly_date_ranges(train_start_date, min_test_start_date, max_te
         if test_end_date > max_test_end_date:
             break
 
-        date_range = (dict(
-            train_start_date = train_start_date,
-            val_start_date = val_start_date,
-            test_start_date = test_start_date,
-            test_end_date = test_end_date,
-        ))
+        if len(date_ranges) == 0:
+            # The first date_range contains the full training period
+            train_start_date = full_train_start_date
+        elif finetune_previous_val:
+            # Use the previous validation range as the training range
+            train_start_date = date_ranges[-1]['val_start_date']
+
+        date_range = dict(
+            train_start_date=train_start_date,
+            val_start_date=val_start_date,
+            test_start_date=test_start_date,
+            test_end_date=test_end_date,
+        )
 
         if return_strings:
             date_range = {k: str(v) for k, v in date_range.items()}
@@ -36,4 +42,5 @@ def generate_quarterly_date_ranges(train_start_date, min_test_start_date, max_te
         date_ranges.append(date_range)
 
         test_start_date = test_end_date
+
     return date_ranges
